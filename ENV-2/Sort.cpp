a21254f58ea239dd8c30caa74da9f8cb8ad590b2
hrantbeg1 <hrantbeglaryan129@gmail.com>
2024-10-08 16:01:47 -0400

    HW2

--- ENV-2/Sort.cpp
+++ ENV-2/Sort.cpp
@@ -1,12 +1,13 @@
 #include "Sort.h"
-void bubble(vector<Complex>& arr){
+#include <algorithm>
+void bubble(std::vector<Complex>& arr){
     size_t n = arr.size();
     bool s;
     for (size_t i = 0; i < n - 1; ++i){
         s = false;
         for (size_t j = 0; j < n - i - 1; ++j){
             if (arr[j].abs() > arr[j + 1].abs()){
-                swap(arr[j], arr[j + 1]);
+               std::swap(arr[j], arr[j + 1]);
                 s = true;
             }
         }
